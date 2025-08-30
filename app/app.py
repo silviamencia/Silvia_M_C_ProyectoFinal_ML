80c87506d77c907408b0abd31c469dd26e06cf98
silviamencia <silvia.mencia@gmail.com>
2025-08-30 11:12:32 +0200

    Update app_2.py

--- app/app.py
+++ app/app.py
@@ -9,6 +9,7 @@ import os
 
 MODEL_PATH = os.path.join("models", "final_model.pkl")
 
+st.title("Predicción: ¿La compañía respondió a tiempo?")
 
 # Verificar que existe antes de cargarlo
 if not os.path.exists(MODEL_PATH):
@@ -23,19 +24,20 @@ with open(MODEL_PATH, "rb") as f:
     modelo = pickle.load(f)
 
 # Título de la app
-st.title("Aplicación de Predicción con Modelo Entrenado")
+#st.title("Aplicación de Predicción con Modelo Entrenado")
 
 st.write("Introduce los valores de las características para obtener la predicción.")
 
 # Ajusta los inputs según las variables de tu dataset
-feature1 = st.number_input("Feature 1", value=0.0)
-feature2 = st.number_input("Feature 2", value=0.0)
-feature3 = st.number_input("Feature 3", value=0.0)
+# Inputs del usuario (categóricos)
+estado = st.text_input("¿En qué estado vives?", value="CDMX")
+producto = st.text_input("Producto relacionado con la queja", value="Tarjeta")
+company = st.text_input("Nombre de la compañía", value="Banco ABC")
 
 # Botón para predecir
 if st.button("Predecir"):
     # Crear arreglo con la entrada
-    entrada = np.array([[feature1, feature2, feature3]])
+    entrada = np.array([[estado, producto, company]])
     
     # Hacer predicción
     prediccion = modelo.predict(entrada)
